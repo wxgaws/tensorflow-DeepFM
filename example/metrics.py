07a0e2b2c66efb484799977afc835a48038d2173
wxgaws <37574580+wxgaws@users.noreply.github.com>
2020-12-30 19:10:51 +0800

    metrics.py：add comments

--- example/metrics.py
+++ example/metrics.py
@@ -3,13 +3,13 @@ import numpy as np
 
 def gini(actual, pred):
     assert (len(actual) == len(pred))
-    all = np.asarray(np.c_[actual, pred, np.arange(len(actual))], dtype=np.float)
-    all = all[np.lexsort((all[:, 2], -1 * all[:, 1]))]
-    totalLosses = all[:, 0].sum()
-    giniSum = all[:, 0].cumsum().sum() / totalLosses
+    all = np.asarray(np.c_[actual, pred, np.arange(len(actual))], dtype=np.float) # actual维度为B*1, all维度为B*3
+    all = all[np.lexsort((all[:, 2], -1 * all[:, 1]))]  #先按pred降序排列，然后按序号升序排列
+    totalLosses = all[:, 0].sum() #正样本量
+    giniSum = all[:, 0].cumsum().sum() / totalLosses #正样本占比曲线下的面积
 
-    giniSum -= (len(actual) + 1) / 2.
-    return giniSum / len(actual)
+    giniSum -= (len(actual) + 1) / 2.   #减去随机分布的结果
+    return giniSum / len(actual)    #按样本量归一化
 
 def gini_norm(actual, pred):
-    return gini(actual, pred) / gini(actual, actual)
+    return gini(actual, pred) / gini(actual, actual)    #相对理想情况的比例
